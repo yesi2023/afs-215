899560c58b64d63209549782e42d888d2b7c8106
yesi2023 <irma.y.rodriguez@bryanuniversity.edu>
2023-04-30 22:19:00 -0500

    Python Kata

--- week1/python_kata.py
+++ week1/python_kata.py
@@ -1,4 +1,4 @@
-def python_kata(num):
+def print_drink(num):
     if num == 1:
         print(1)
     elif num == 2:
@@ -14,10 +14,12 @@ def python_kata(num):
     elif num % 5 == 0:
         print("Coke")
 
-python_kata(1)   # prints "1"
-python_kata(2)   # prints "2"
-python_kata(3)   # prints "Pepsi"
-python_kata(5)   # prints "Coke"
-python_kata(6)   # prints "Pepsi"
-python_kata(10)  # prints "Coke"
-python_kata(15)  # prints "PepsiCoke"
+# Test cases for the print_drink function:
+
+print_drink(1)   # prints "1"
+print_drink(2)   # prints "2"
+print_drink(3)   # prints "Pepsi"
+print_drink(5)   # prints "Coke"
+print_drink(6)   # prints "Pepsi"
+print_drink(10)  # prints "Coke"
+print_drink(15)  # prints "PepsiCoke"
